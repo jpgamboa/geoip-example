12f81ef81e65932061284192453cf0d9340ee701
jpgamboa <jgamboa@gmail.com>
2015-10-08 17:04:36 -0500

    First commit

---
+++ geoip.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: John Gamboa's GEO IP Redirects
+Description: Redirecting visitor traffic based on geo-location using the WPEngine GEO IP feature
+Version: 1.0
+License: GPLv2
+*/
+
+function country_geo_redirect() {
+
+$country = getenv('HTTP_GEOIP_COUNTRY_CODE');
+
+if ( $country == "US" ) {
+
+wp_redirect( 'https://en.wikipedia.org/wiki/United_States', 301 );
+
+     exit;
+
+} else if ( $country == "BR" ) {
+
+wp_redirect( 'https://en.wikipedia.org/wiki/Brazil', 301 );
+
+     exit;
+
+}else if ( $country !== "(US|BR)" ) {
+
+wp_redirect( 'https://en.wikipedia.org/wiki/Earth', 301 );
+
+     exit;
+
+}
+
+}
+
+add_action('init', 'country_geo_redirect');
